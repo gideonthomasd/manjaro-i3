b6dc43d137c8d61597a0e0f1f52f2ad8a7a80373
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-12-29 14:08:44 +0000

    Add files via upload

---
+++ 200_Openbox.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo pacman -S openbox obconf tint2 obmenu-generator feh jgmenu lxappearance lxtask gtk2-perl appimagelauncher lxsession pcmanfm xfce4-terminal conky rofi ttf-ubuntu-font-family geany
+
+mkdir -p ~/.config/conky
+cp sysinfo_mbcolor.conkyrc ~/.config/conky
+
+cd tint2
+cd scripts
+chmod +x volumettf
+cd ..
+cd ..
+
+mkdir -p ~/.config/tint2
+cd tint2
+cp -r * ~/.config/tint2
+cd ..
+
+mkdir -p ~/.config/jgmenu
+cp jgmenurc ~/.config/jgmenu
+
+cp log.sh ~/log.sh
+
+
+yay -S nerd-fonts-mononoki
+obmenu-generator -p -i
+
+
---
+++ openbox/kill.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 10
+pkill pamac-tray
+pkill blueman-applet
+pkill msm_notifier
+pkill applet.py
+killall xdg-desktop-portal-gnome
+killall goa-daemon
