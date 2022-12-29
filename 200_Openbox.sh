b38b65a320182219f9d11e82dc2d3cd28569d874
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-12-29 14:28:51 +0000

    Update 200_Openbox.sh

--- 200_Openbox.sh
+++ 200_Openbox.sh
@@ -2,6 +2,12 @@
 
 sudo pacman -S openbox obconf tint2 obmenu-generator feh jgmenu lxappearance lxtask gtk2-perl appimagelauncher lxsession pcmanfm xfce4-terminal conky rofi ttf-ubuntu-font-family geany
 
+tar -xzvf obmenugenerator.tar.gz
+mkdir -p ~/.config/obmenu-generator
+cd obmenu-generator
+cp -r * ~/.config/obmenu-generator
+cd ..
+
 mkdir -p ~/.config/conky
 cp sysinfo_mbcolor.conkyrc ~/.config/conky
 
